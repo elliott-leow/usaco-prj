cb5c1de88a02b38fb324a0f8c1e1852090053b25
Vindou <nejirebot@users.noreply.github.com>
2020-05-24 15:19:33 -0700

    more messing around lol

--- USACO.cpp
+++ USACO.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main ()
 {
-  int n = 10;
-
-for (int n = 50; n>0; n--){
-    cout << n << ",";
-
-    }
-
-    cout << "ur mom";
+  int x;
+  cin >> x;
+  if (x == 1) {
+    cout << "x is 1";
+  }
+  else if (x == 2) {
+    cout << "x is 2";
+  }
+  else if (x==3) {
+    cout << "x is 3";
+  }
+  else {
+    cout << "SHUT UP";
+  }
 }
